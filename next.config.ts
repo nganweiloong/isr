c2f6f2a27453947437d53d44add44e1ab459aff8
Wei Loong <weiloong@netizenexperience.com>
2025-07-09 15:12:41 +0800

    add sst build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  productionBrowserSourceMaps: true,
+  webpack(config, { dev }) {
+    if (!dev) {
+      config.optimization.minimize = false;
+    }
+    return config;
+  },
 };
 
 export default nextConfig;
